ce7155fe9556e0dc9e29369ffae3f04f16e455ef
Officialthesky <surajkumaryadavvvv@gmail.com>
2022-02-22 13:04:23 +0530

    Details Adding in Sideheader

--- src/components/Sideheader/index.js
+++ src/components/Sideheader/index.js
@@ -9,7 +9,22 @@ export default function Sideheader({ toggleSideBar }) {
   return (
     <div className="sideheader-container">
       <div className="sideheader-inner-container">
-        <h1 onClick={toggle}>close</h1>
+        <div className="close-sideheader">
+        <h1 onClick={toggle}>X</h1>
+        </div>
+        <div className="sideheader-navbar">
+          <ul>
+            <li>Create</li>
+            <li>Listen</li>
+            <li>Products</li>
+            <li>Features</li>
+            <li>FAQ</li>
+            <li>Add/Remove</li>
+         </ul>
+        </div>
+        <div className="sideheader-button">
+        <button>How to Create</button>
+        </div>
       </div>
     </div>
   );
